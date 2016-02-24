2b19b65fa11135fe5a80213c1fcedcb9d87f7ba1
blackcata <blackcata109@gmail.com>
2016-02-24 15:58:30 +0900

    Add the Projection method main and module files

---
+++ Projection_main.f90
@@ -0,0 +1,19 @@
+!----------------------------------------------------------------------------
+!
+!  PROGRAM: Projection_main.f90
+!
+!  PURPOSE: To solve the cavity problem, projection method was used to 
+!           solve the Navier-Stokes equation. 
+!
+!----------------------------------------------------------------------------
+    
+        PROGRAM Projection_main
+
+            IMPLICIT NONE
+            INTEGER :: it, i, j, k
+            REAL(KIND=8),PARAMETER :: pi
+            
+            pi = atan(1.0) * 4
+
+        END PROGRAM Projection_main
+
---
+++ Projection_module.f90
@@ -0,0 +1,18 @@
+!----------------------------------------------------------------------------
+!
+!  PROGRAM: Projection_main.f90
+!
+!  PURPOSE: Modules for Projection method
+!
+!   log 
+!   2016.02.24 First make a module file and add file_name, path_name 
+!
+!----------------------------------------------------------------------------
+        MODULE projection_module
+            
+            CHARACTER(LEN=65) :: file_name, path_name
+            
+            SAVE
+            
+        END MODULE projection_module
+    
