b22b5ce847ebebb1735b67a3469fd3addd89e9e9
blackcata <blackcata109@gmail.com>
2016-03-06 14:19:05 +0900

    Test the residual module

--- Projection_main.f90
+++ Projection_main.f90
@@ -15,16 +15,26 @@
                 ONLY : file_name, path_name
             
             USE projection_module,                                                &
-                ONLY : Nx, Ny, dx, dy
+                ONLY : Nx, Ny, dx, dy, U,V, Rx, Ry, Hx, Hy, Lx, Ly
 
             IMPLICIT NONE
-            
+            INTEGER :: i,j
+
             path_name = 'RESULT' 
             CALL SYSTEM('mkdir '//TRIM(path_name))
             CALL SETUP
             CALL RESI
 
             OPEN(100,FILE=TRIM(path_name)//'/TEST.plt',FORM='FORMATTED',STATUS='REPLACE')
+            WRITE(100,*) 'VARIABLES = "x","y","Rx","Hx","Lx"'
+            WRITE(100,200) Nx-1, Ny
+            200 FORMAT('ZONE',2X,'I=',I3,2X,'J=',I3,2X,'F=Point')
+
+            DO j = 1, Ny
+              DO i = 1, Nx-1
+                WRITE(100,*) i*dx, j*dy, Rx(i,j), Hx(i,j), Lx(i,j)
+              END DO
+            END DO
             CLOSE(100) 
 
             
