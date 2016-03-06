540dfb87dc9040a1bc071551ee67f874fafe2195
blackcata <blackcata109@gmail.com>
2016-03-06 14:30:37 +0900

    Changed the files name and algorthm getdu -> getuh

--- Projection_getuh.f90
+++ Projection_getuh.f90
@@ -1,26 +1,34 @@
 !-----------------------------------------------------------------------------------!
 !
-!  PROGRAM: Projection_getvh.f90
+!  PROGRAM: Projection_getuh.f90
 !
-!  PURPOSE: Get VH(V hat which is addition between v and dv
+!  PURPOSE: Get UH(U hat which is addition between u and delta u)
 !
 !                                                                2016.03.04 K.Noh
 !                                                   
 !   log 
 !   2016.03.04 Add getdu and getdv code for 1st process of projection method
-!   2016.03.06 Change the files name and algorithm of this subroutine
+!   2016.03.06 Modified the files name and alogrithm to getdu -> getuh
 !
 !-----------------------------------------------------------------------------------!
     
-        SUBROUTINE GETVH
+        SUBROUTINE GETUH
             
             USE projection_module,                                              &
-                ONLY : Nx, Ny, dx, dy
+                ONLY : Nx, Ny, dx, dy, Re, dt
             
             USE projection_module,                                              &
-                ONLY : U, V, Uhat, Vhat
+                ONLY : U, V, Rx
             
             IMPLICIT NONE
+            INTEGER :: i,j
+            REAL(KIND=8) :: c1, c2
+            REAL(KIND=8) :: D(1:Nx-1,1:Ny), du(1:Nx-1,1:Ny)
+
+            c1 = dt / (2*Re*dx**2)
+            c2 = dt / (2*Re*dy**2)
+
+
             
-        END SUBROUTINE GETVH
+        END SUBROUTINE GETUH
     
